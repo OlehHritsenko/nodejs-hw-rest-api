cc8b18ded407d63a0e7765763cbbddb814af04d0
OlehHritsenko <grolan12345@gmail.com>
2023-04-17 23:50:18 +0300

    hw03-mongodb

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -2,9 +2,11 @@ const createError = require("http-errors");
 
 const validation = (schema) => {
   return (req, res, next) => {
+    // if there is no body
     if (Object.keys(req.body).length === 0) {
       return next(createError(400, "Missing fields"));
     }
+    // other errors in requests
     const { error } = schema.validate(req.body);
     if (error) {
       return next(createError(400, error.message));
